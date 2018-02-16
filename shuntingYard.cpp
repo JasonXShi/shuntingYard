7f843bc814b94b62a3a4b9136a6cd4c693f544c1
Jason Shi <jasonxshi2002@gmail.com>
2018-02-16 12:23:41 -0800

    fds

--- shuntingYard.cpp
+++ shuntingYard.cpp
@@ -6,19 +6,39 @@
 #include <cstring>
 #include "Node.h"
 #include <iostream>
+#include <vector>
 #include <cstdlib>
+#include <iterator>
+#include <map>
 using namespace std; 
 
 int main(){
     while(1){
+
         char* input = new char[100];
         cout << "Enter an expression to be outputted as postfix (Insert spaces between each token):" << endl;
         cin.getline(input, 100);
         vector<char*> v;
-        while(){
-            v.push_back(strtok(" "));
+        char* token=strtok(input, " ");
+        while(token != NULL){
+            v.push_back(token);
+            token = strtok(NULL, " ");
         }
-
+        map<char, int> map;
+        map['-'] = 2;
+        map['+'] = 2;
+        map['*'] = 3; 
+        map['/'] = 3;
+        map['^'] = 4;
+        map['('] = 5;
+        map[')'] = 5;
+        for(vector<char*>::iterator i = v.begin(); i!= v.end(); advance(i, 1)){
+            if(isdigit(v.at(*i))){
+        
+            }else{
+             
+            }
+        }
         //output as postfix
         //go through the char[] and push things to their places
                 //if is a digit
@@ -29,11 +49,11 @@ int main(){
             cout << "Convert to Prefix, Infix, Postfix, or Exit?" << endl << "(PRE, IN, POST, EXIT)" << endl;
             cin.getline(input2, 5);
             if(strcmp(input2, "PRE")==0){
-            
+                
             }else if(strcmp(input2, "IN")==0){
                 
             }else if(strcmp(input2, "POST")==0){
-                //print out the original thing
+                
             }else if(strcmp(input2, "EXIT")==0){
                 break;
             }
